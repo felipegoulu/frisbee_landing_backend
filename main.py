129f9dc0fd5d64332fd0d71bf557e3f7507b7790
Felipe Goulu <felipegoulu16@gmail.com>
2025-01-21 14:23:37 -0300

    allowing origins from vercel and local

--- main.py
+++ main.py
@@ -15,11 +15,10 @@ app = FastAPI()
 # Enable CORS
 app.add_middleware(
     CORSMiddleware,
-    #allow_origins=[
-    #    "https://frizbee.site.vercel.app", # recibe api calls el site en vercel o del local host
-    #    "http://localhost:3000"
-    #],  # Add your frontend URL
-    allow_origins=["*"],
+    allow_origins=[
+        "https://frizbee.site.vercel.app", # recibe api calls el site en vercel o del local host
+        "http://localhost:3000"
+    ],  # Add your frontend URL
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
